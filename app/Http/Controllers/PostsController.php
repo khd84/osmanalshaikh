5712b00759adc019b99b040f6debca615eaa14c3
khd84 <khad8430@gmail.com>
2023-05-18 17:42:23 +0300

    post views

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
+
 class PostsController extends Controller
 {
     /**
@@ -13,7 +15,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('blog.index');
+
+        return view('blog.index') ->with('posts',Post:: get())  ;
     }
 
 
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,94 +2,31 @@
 
 @section('content')
 
-<div class=" container m-auto text-center py-10">
+<!--{{$posts}} -->
 
-    <h1 class=" text-6xl font-bold">All Posts</h1>
-</div>
 
-<div class=" container sm:grid  grid-cols-2 gap-15 mx-auto py-15 px-5 justify-between items-center border-b border-cool-gray-300">
+@foreach ($posts as $post )
+    <div class=" container sm:grid  grid-cols-2 gap-15 mx-auto py-15 px-5 justify-between items-center border-b border-cool-gray-300">
         <div>
-            <img class=" object-cover" src="https://picsum.photos/id/343/960/620" alt="">
+            <img class=" object-cover" src="{{$post-> image_path}}" alt="">
         </div>
 
         <div>
             <h2 class=" text-gray-700 font-bold text-4xl py-5 pt-0">
-                How To Talk Like Commonist
+                {{$post-> title}}
             </h2>
             <span>
-                By : <span class=" text-gray-500 italic">Othman Alshaikh</span>
-                    <p class="py-10 text-gray-500 text-sm leading-5">
-                        بل مدة ليست بطويلة ڪُنت في دار الأجنحة للطباعة والنشر ،لزيارة صديقي أنس وإقتناء بعض الڪتب والتسامر ڪالعادة .وبعد أخذ ما أريد طلبت منه أن يرشح لي كتاب على ذوقه لأضيفه لمحصلت مالدي ، وقبل إجابته دلف الينا الاستاذ متوڪل زروق وبادلنا أطراف الحديث فيما يتعلق بالڪتب ،وبينما نحن كذلك وقعت عيني على (تلصص) فسألته مارأيك بكتابات عثمان شيخ فأنا لم أقرئ له من قبل ، أجاب أنصح به ،لديه أسلوب جميل في الكتابة ،فأخذته وهممت بعد وقت بالإنصراف.
+                By : <span class=" text-gray-500 italic">{{$post->user->name}}</span>
+                On : <span class=" text-gray-500 italic">{{ date( 'd-m-Y h:i:s A' , $post->updated_at ) }}</span>
 
-                    </p>
+                <p class="py-10 text-gray-500 text-sm leading-5 font-serif"> {{$post-> description}} </p>
 
-                    <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase text-l place-self-start" href="/">Read More</a>
+                    <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase text-l place-self-start" href="/blog/{{$post->slug}}">Read More</a>
             </span>
         </div>
 </div>
+@endforeach
 
-<div class=" container sm:grid  grid-cols-2 gap-15 mx-auto py-15 px-5 justify-between items-center border-b border-cool-gray-300">
-    <div>
-        <img class=" object-cover" src="https://picsum.photos/id/89/960/620" alt="">
-    </div>
-
-    <div>
-        <h2 class=" text-gray-700 font-bold text-4xl py-5 md:pt-0">
-            How To Talk Like Commonist
-        </h2>
-        <span>
-            By : <span class=" text-gray-500 italic">Othman Alshaikh</span>
-                <p class="py-10 text-gray-500 text-sm leading-5">
-                    بل مدة ليست بطويلة ڪُنت في دار الأجنحة للطباعة والنشر ،لزيارة صديقي أنس وإقتناء بعض الڪتب والتسامر ڪالعادة .وبعد أخذ ما أريد طلبت منه أن يرشح لي كتاب على ذوقه لأضيفه لمحصلت مالدي ، وقبل إجابته دلف الينا الاستاذ متوڪل زروق وبادلنا أطراف الحديث فيما يتعلق بالڪتب ،وبينما نحن كذلك وقعت عيني على (تلصص) فسألته مارأيك بكتابات عثمان شيخ فأنا لم أقرئ له من قبل ، أجاب أنصح به ،لديه أسلوب جميل في الكتابة ،فأخذته وهممت بعد وقت بالإنصراف.
-
-                </p>
-
-                <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase text-l place-self-start" href="/">Read More</a>
-        </span>
-    </div>
-</div>
-
-<div class=" container sm:grid  grid-cols-2 gap-15 mx-auto py-15 px-5 justify-between items-center border-b border-cool-gray-300">
-    <div>
-        <img class=" object-cover" src="https://picsum.photos/id/55/960/620" alt="">
-    </div>
-
-    <div>
-        <h2 class=" text-gray-700 font-bold text-4xl py-5 md:pt-0">
-            How To Talk Like Commonist
-        </h2>
-        <span>
-            By : <span class=" text-gray-500 italic">Othman Alshaikh</span>
-                <p class="py-10 text-gray-500 text-sm leading-5">
-                    بل مدة ليست بطويلة ڪُنت في دار الأجنحة للطباعة والنشر ،لزيارة صديقي أنس وإقتناء بعض الڪتب والتسامر ڪالعادة .وبعد أخذ ما أريد طلبت منه أن يرشح لي كتاب على ذوقه لأضيفه لمحصلت مالدي ، وقبل إجابته دلف الينا الاستاذ متوڪل زروق وبادلنا أطراف الحديث فيما يتعلق بالڪتب ،وبينما نحن كذلك وقعت عيني على (تلصص) فسألته مارأيك بكتابات عثمان شيخ فأنا لم أقرئ له من قبل ، أجاب أنصح به ،لديه أسلوب جميل في الكتابة ،فأخذته وهممت بعد وقت بالإنصراف.
-
-                </p>
-
-                <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase text-l place-self-start" href="/">Read More</a>
-        </span>
-    </div>
-</div>
-
-<div class=" container sm:grid  grid-cols-2 gap-15 mx-auto py-15 px-5 justify-between items-center border-b border-cool-gray-300">
-    <div>
-        <img class=" object-cover" src="https://picsum.photos/id/100/960/620" alt="">
-    </div>
-
-    <div>
-        <h2 class=" text-gray-700 font-bold text-4xl py-5 md:pt-0">
-            How To Talk Like Commonist
-        </h2>
-        <span>
-            By : <span class=" text-gray-500 italic">Othman Alshaikh</span>
-                <p class="py-10 text-gray-500 text-sm leading-5">
-                    بل مدة ليست بطويلة ڪُنت في دار الأجنحة للطباعة والنشر ،لزيارة صديقي أنس وإقتناء بعض الڪتب والتسامر ڪالعادة .وبعد أخذ ما أريد طلبت منه أن يرشح لي كتاب على ذوقه لأضيفه لمحصلت مالدي ، وقبل إجابته دلف الينا الاستاذ متوڪل زروق وبادلنا أطراف الحديث فيما يتعلق بالڪتب ،وبينما نحن كذلك وقعت عيني على (تلصص) فسألته مارأيك بكتابات عثمان شيخ فأنا لم أقرئ له من قبل ، أجاب أنصح به ،لديه أسلوب جميل في الكتابة ،فأخذته وهممت بعد وقت بالإنصراف.
-
-                </p>
-
-                <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase text-l place-self-start" href="/">Read More</a>
-        </span>
-    </div>
-</div>
 
 
 @endsection
