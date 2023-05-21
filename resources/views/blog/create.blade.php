a8eaf6eb55784744a4911c662954ade486329034
khd84 <khad8430@gmail.com>
2023-05-21 10:36:06 +0300

    create blade

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -6,7 +6,17 @@
 </div>
 
 <div class=" container m-auto text-center pt-15 pb-5">
-<form action="/blog" method="POST"></form>
+<form action="/blog" method="POST">
+
+    <input
+    type= "text"
+    name= "title"
+    placeholder= "Title"
+    class = "w-full h-20 text-6xl rounded-lg shadow-lg border-b pb-15 mb-5 "
+    >
+
+
+</form>
 </div>
 
 @endsection
