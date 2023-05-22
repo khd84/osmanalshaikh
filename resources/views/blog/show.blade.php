7ce1f9ec7a869e1e9dd9cc4817fac0c3ff8bc34b
khd84 <khad8430@gmail.com>
2023-05-22 19:01:36 +0300

    prelast

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -13,15 +13,7 @@
     <div class="mt-2 mb-4 text-sm">
         {{session()->get('message')}}
     </div>
-    {{-- <div class="flex">
-      <button type="button" class="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <svg aria-hidden="true" class="-ml-0.5 mr-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"></path></svg>
-        View more
-      </button>
-      <button type="button" class="text-blue-800 bg-transparent border border-blue-800 hover:bg-blue-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-blue-600 dark:border-blue-600 dark:text-blue-400 dark:hover:text-white dark:focus:ring-blue-800" data-dismiss-target="#alert-additional-content-1" aria-label="Close">
-        Dismiss
-      </button>
-    </div> --}}
+
   </div>
   @endif
 <div class=" container m-auto text-center pt-15 pb-5">
@@ -46,12 +38,36 @@
     <div class=" text-l text-gray-700 py-8 leading-6">
             {{$post->description}}
     </div>
-  @if(Auth::user() && Auth::user()->id == $post->user_id )
-    <div class=" px-10">
-        <a class=" px-5 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" href="/blog/{{$post->slug}}/edit">تعديل الموضوع</a>
 
-    </div>
-@endif
+
+    @if(Auth::user() && Auth::user()->id == $post->user_id )
+    <a href="/blog/{{$post->slug}}/edit" class=" px-5 text-white
+     bg-green-700 hover:bg-green-800
+     focus:outline-none focus:ring-4
+     focus:ring-green-300 font-medium
+     rounded-full text-sm py-2.5
+     text-center mr-2 mb-2 dark:bg-green-600
+     dark:hover:bg-green-700
+     dark:focus:ring-green-800" >تعديل الموضوع</a>
+
+     <form action="/blog/{{$post->slug}}" method="POST" class=" inline-block p-x15 ">
+      @csrf
+      @method('delete')
+
+      <button type="submit" class=" px-5 text-white bg-red-700
+      hover:bg-red-800 focus:outline-none
+      focus:ring-4 focus:ring-red-300
+      font-medium rounded-full text-sm
+      py-2.5 text-center mr-2 mb-2 dark:bg-red-600
+      dark:hover:bg-red-700
+      dark:focus:ring-red-800 inline-block"
+      >حذف الموضوع</button>
+     </form>
+
+    @endif
+
 </div>
+
+
 
 @endsection
