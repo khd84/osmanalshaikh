d71a450db1c3a24205d4e861ef4a77d99666ca25
khd84 <khad8430@gmail.com>
2023-05-23 17:24:29 +0300

    latest post query

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,11 +1,6 @@
-
-
 @extends('layouts.app')
-
 @section('content')
 @if (session()->has('message'))
-
-
 <div id="alert-additional-content-1" class="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
     <div class="flex items-center">
       <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
@@ -15,7 +10,6 @@
     <div class="mt-2 mb-4 text-sm">
         {{session()->get('message')}}
     </div>
-
   </div>
   @endif
 <!--{{$posts}} -->
@@ -28,8 +22,6 @@
 
     </div>
 @endif
-
-
 @foreach ($posts as $post )
     <div class=" container sm:grid  grid-cols-2 gap-15 mx-auto py-15 px-5 justify-between items-center border-b border-cool-gray-300">
         <div>
@@ -49,15 +41,10 @@
             <span>
                 By : <span class=" text-gray-500 italic">{{$post->user->name}}</span>
                 On : <span class=" text-gray-500 italic">{{  $post->updated_at->format('Y-m-d H:i:s') }}</span>
-
-                <p class="py-10 text-gray-500 text-sm leading-5 font-serif"> {{$post-> description}} </p>
-
+                    <p class="py-10 text-gray-500 text-sm leading-5 font-serif"> {{Str::limit($post-> description, 1000 )}} </p>
                     <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase text-l place-self-start" href="/blog/{{$post->slug}}">Read More</a>
             </span>
         </div>
 </div>
 @endforeach
-
-
-
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,5 @@
+
 <footer class="bg-gray-900 py-15 mt-20">
-
-
-
     <div class="container mx-auto flex justify-between items-center px-6">
 
         <div>
@@ -31,17 +29,17 @@
                     <li class="pb-2 text-white">
                         <a class="hover:text-gray-600 transition duration-300" href="/">الدعامة في دهاليز التأهيل</a></li>
                     <li class="pb-2 text-white">
-                        <a class="hover:text-gray-600 transition duration-300" href="/">حين اصبح الدعامي وديعا واشتغل تجاة عدة</a></li>
+                        <a class="hover:text-gray-600 transition duration-300" href="/">{{ $tit }}</a></li>
                         --}}
                      @php
                         $topTitles = \App\Models\Post::orderBy('title', 'asc')
                                         ->limit(3)
                                         ->pluck('title');
                       @endphp
-   @dd($topTitles);
+
                       <ol class="pt-4 text-white">
-                        @foreach ($topTitles as $title)
-                            <li class="pb-2 text-white >{{ $title }}</li>
+                        @foreach ($topTitles as $tit)
+                            <li class="pb-2 text-white"><a class="hover:text-gray-600 transition duration-300" href="/blog/{{Str::slug($tit,'-')}}" >{{ $tit }}</a></li>
                         @endforeach
                       </ol>
         </div>
